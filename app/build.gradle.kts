b8f520e8ad2447abcf7cdd5084447568f6059f73
Joseph White <48302349+jwhi0207@users.noreply.github.com>
2026-03-20 15:12:14 -0500

    workflow changes. build changes
    workflow changes. build changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.bennybokki.frientrip"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = (System.getenv("GITHUB_RUN_NUMBER")?.toIntOrNull() ?: 1)
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
